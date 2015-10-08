f5a8aa27f0544360f67b30c8f8c258cf1a7d7baa
Hiren Parmar <hirenparmar_r@yahoo.com>
2015-10-07 20:49:33 -0700

    resolved issues with Product Entity

--- jpa-basics/src/com/learn/executables/ProductMainApp.java
+++ jpa-basics/src/com/learn/executables/ProductMainApp.java
@@ -6,10 +6,15 @@ import javax.persistence.Persistence;
 
 import com.learn.intermediate.jpa.Product;
 
-public class ProductMainClass {
+public class ProductMainApp {
 	
 	public static void main(String[] args) {
 	
+		//was getting error
+		//org.apache.openjpa.persistence.ArgumentException: 
+		//No metadata was found for type . ''The class is not enhanced
+		//resolved it by adding Entity to persistence.xml
+		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("mysql-testpu");
 		EntityManager em = emf.createEntityManager();
 		Product product = em.find(Product.class, 2000);
